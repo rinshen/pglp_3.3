4a7d139e6e6620bb61caf7b1e05a41df25037c91
rinshen <60706938+rinshen@users.noreply.github.com>
2020-05-24 23:25:58 +0200

    code correct

---
+++ code.java
@@ -0,0 +1,9 @@
+Class RobotStatique {
+   private Position position; 
+   private Direction direction;
+}
+
+Class Robot extends RobotStatique {
+  public void tourne();
+  public void avance();
+ }
